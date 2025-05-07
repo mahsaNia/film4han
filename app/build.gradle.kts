82814779809375d781c0998b68ad780ee25cb005
mahsa <mahsaniazi1228@gmail.com>
2025-05-07 15:45:01 +0330

    kapt is installed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    kotlin("kapt") // ✅ این خط باید اینجا باشه
 }
 
 android {
@@ -49,6 +51,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation("com.google.dagger:hilt-android:2.44")
+
+// کامپایل‌کننده Hilt (برای annotationها)
+    kapt("com.google.dagger:hilt-compiler:2.44")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
